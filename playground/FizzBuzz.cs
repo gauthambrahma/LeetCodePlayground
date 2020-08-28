a28a80f1b466d3a8f07d70679210502881e06cd1
gauthambrahma <gauthamponnaganti@gmail.com>
2020-08-28 15:57:12 -0500

    add solution.add driver class for current and future projects

---
+++ playground/FizzBuzz.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace playground
+{
+    public class FizzBuzz
+    {
+      
+        public IList<string> printFizzBuzz(int n)
+        {
+
+            IList<string> result = new List<String>();
+            if (n < 1)
+            {
+                return result;
+            }
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    result.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    result.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    result.Add("Buzz");
+                }
+                else
+                {
+                    result.Add(.ToString());
+                }
+            }
+
+            return result;
+        }
+    }    
+}
