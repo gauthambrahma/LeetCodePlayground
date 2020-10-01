c3989bb9630bc5bccfa7275ede62009a35067efa
gauthambrahma <gautham.ponnaganti@okstate.edu>
2020-10-01 14:37:57 -0500

    no of recent calls code

--- PlaygroudTest/RecentCounterTest.cs
+++ PlaygroudTest/RecentCounterTest.cs
@@ -1,10 +1,29 @@
 ﻿using System;
+using playground;
+using Xunit;
+
 namespace PlaygroudTest
 {
     public class RecentCounterTest
     {
+        private readonly RecentCounter recentCounter;
+
         public RecentCounterTest()
         {
+            recentCounter = new RecentCounter();
+        }
+
+        [Fact]
+        public void input1()
+        {
+            var result = recentCounter.Ping(1);
+            var result2 = recentCounter.Ping(100);
+            var result3 = recentCounter.Ping(3001);
+            var result4 = recentCounter.Ping(3002);
+            Assert.Equal(1, result);
+            Assert.Equal(2, result2);
+            Assert.Equal(3, result3);
+            Assert.Equal(3, result4);
         }
     }
 }
--- playground/RecentCounter.cs
+++ playground/RecentCounter.cs
@@ -1,10 +1,34 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace playground
 {
-    public class No_of_recent_calls
+    public class RecentCounter
     {
-        public No_of_recent_calls()
+
+        public List<int> itemsInRange = new List<int>();
+        int currentMin = 0;
+        int currentMax = 0;
+        public RecentCounter()
         {
+
+        }
+
+        public int Ping(int t)
+        {
+            currentMin = t - 3000;
+            currentMax = t;
+            if (itemsInRange.Count == 0)
+            {
+                itemsInRange.Add(t);
+            }
+            else
+            {
+                itemsInRange.RemoveAll(item => item < currentMin || item > currentMax);
+                itemsInRange.Add(t);
+            }
+            return itemsInRange.Count;
         }
     }
 }
