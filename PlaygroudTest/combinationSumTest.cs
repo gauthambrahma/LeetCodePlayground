273487284db0b7a7a0e5fd7815b2b10ec3737ca1
gauthambrahma <gautham.ponnaganti@okstate.edu>
2020-10-02 23:27:47 -0500

    add solution-combination sum

---
+++ PlaygroudTest/combinationSumTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using playground;
+using Xunit;
+
+namespace PlaygroudTest
+{
+    public class combinationSumTest
+    {
+        private readonly combinationSum combinationSum;
+        IList<List<int>> expectedResult = new List<List<int>>();
+
+        public combinationSumTest()
+        {
+            combinationSum = new combinationSum();
+        }
+
+        [Fact]
+        public void input1()
+        {
+            int[] candidates = new int[] { 2, 3, 6, 7 };
+            var result = combinationSum.CombinationSum(candidates, 7);
+            List<int> input = new List<int> { 2, 2, 3 };
+            expectedResult.Add(new List<int>(input));
+            input = new List<int> { 7 };
+            expectedResult.Add(new List<int>(input));
+	        Assert.Equal(expectedResult, result);
+        }
+        [Fact]
+        public void input2()
+        {
+            int[] candidates = new int[] { 2, 3, 5 };
+            var result = combinationSum.CombinationSum(candidates, 8);
+            List<int> input = new List<int> { 2, 2, 2,2};
+            expectedResult.Add(new List<int>(input));
+            input = new List<int> { 2,3,3 };
+            expectedResult.Add(new List<int>(input));
+            input = new List<int> { 3, 5 };
+            expectedResult.Add(new List<int>(input));
+            Assert.Equal(expectedResult, result);
+        }
+        [Fact]
+        public void input3()
+        {
+            int[] candidates = new int[] { 2 };
+            var result = combinationSum.CombinationSum(candidates, 1);
+            Assert.Equal(expectedResult, result);
+        }
+        [Fact]
+        public void input4()
+        {
+            int[] candidates = new int[] { 1 };
+            var result = combinationSum.CombinationSum(candidates, 1);
+            List<int> input = new List<int>{1};
+            expectedResult.Add(new List<int>(input));
+            Assert.Equal(expectedResult, result);
+        }
+        [Fact]
+        public void input5()
+        {
+            int[] candidates = new int[] { 1 };
+            var result = combinationSum.CombinationSum(candidates, 2);
+            List<int> input = new List<int> { 1,1 };
+            expectedResult.Add(new List<int>(input));
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ playground/combinationSum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace playground
+{
+    public class combinationSum
+    {
+        public combinationSum()
+        {
+
+        }
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            List<int> subresult = new List<int>();
+            constructSum(target, subresult, result, candidates[0], candidates, 0);
+            return result;
+        }
+        public bool constructSum(int sum, IList<int> subresult, IList<IList<int>> result, int number, int[] candidates, int index)
+        {
+            if (sum < 0)
+            {
+                return false;
+            }
+            if (sum == 0)
+            {
+                result.Add(new List<int>(subresult));
+            }
+
+            if (sum > 0)
+            {
+                for (int i = index; i < candidates.Length; i++)
+                {
+                    int item = candidates[i];
+                    subresult.Add(item);
+                    constructSum(sum - item, subresult, result, item, candidates, i);
+                    subresult.RemoveAt(subresult.Count - 1);
+                }
+            }
+            return true;
+        }
+    }
+}
